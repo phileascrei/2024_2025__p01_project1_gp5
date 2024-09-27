a76ce6da9914e9a6427e399c180b324d5badf37f
r3nau1 <renaudpointurier1@gmail.com>
2024-09-27 11:45:29 +0200

    updates main
    mise en départ du projet ( recopiage sur le prof )

---
+++ main.py
@@ -0,0 +1,17 @@
+from import tools * 
+from import data * 
+
+def bin_dec_hex_to_bin_dec_hex (init_number, init_base, target_base) :
+    target_number = None     
+    return target_number  
+
+assert bin_dec_hex_to_bin_dec_hex ("101",2,10) == "5" 
+
+def do_the_job (): 
+    init_number = ask_for_the_init_number ()
+    init_base = ask_for_the_init_base ()
+    target_base = ask_for_the_target_base ()
+    target_number = \
+    bin_dec_hex_to_bin_dec_hex (init_number, \
+                                init_base,\
+                                 target_base)
