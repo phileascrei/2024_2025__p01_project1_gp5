17443ff6273f28fdd6f672705feae1e9d915d064
phileascrei <phileascreignou22@gmail.com>
2024-09-27 12:11:24 +0200

    Update tools.py
    ajout d'un sommaire qui dit ou se trouve chaque fonctions

--- tools.py
+++ tools.py
@@ -1,3 +1,9 @@
 from tools_renaud import *
 from tools_phileas import *
 from tools_arsene import * 
+
+# ask_for_the_init_number () -> tools_phileas.py
+
+# ask_for_the_init_base () -> tools_renaud.py
+
+# ask_for_the_target_base () -> tools_arsene.py
