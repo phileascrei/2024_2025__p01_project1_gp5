a3f5a3e3ac87f659ff3975425b10fe1b125cb5fa
phileascrei <phileascreignou22@gmail.com>
2024-09-30 13:33:27 +0200

    Update tools_phileas.py
    correction de la fonction is_a_valid_number et de la definition de la variable number

--- tools_phileas.py
+++ tools_phileas.py
@@ -1,10 +1,11 @@
-def is_a_valid_number() :
+def is_a_valid_number(number) :
     i = 0
     is_a_valid_char = True
     while is_a_valid_char() :
         is_a_valid_char = check_char_number_validity (number [i])
         i = i + 1
     return is_a_valid_char
+
 
 def check_char_number_validity () :
     pass
