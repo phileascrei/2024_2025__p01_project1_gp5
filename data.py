9ea2373b56b2dc0c8297a1f49efce57b795626c3
arsene432 <arsene.tessier2008@gmail.com>
2024-09-30 15:10:54 +0200

    update data.py et tool_arsene.py
    ajout de la liste des bases accepter
    creation de la fonction is_a_valid_base()

--- data.py
+++ data.py
@@ -2,7 +2,9 @@ ask_for_the_init_number_text = "Le nombre ? : "
 ask_again_for_the_init_number_text = "Le nombre put*** ! : "
 
 ask_for_the_target_base_text = "La base cible ? : "
-ask_again_for_the_init_number_text = "La base put*** ! : "
+ask_again_for_the_target_base_text = "La base put*** ! : "
+
+list_base_accepted = ["2", "10", "16"]
 
 bin_number_valid_chars = [0,1]
 dec_number_valid_chars = bin_number_valid_chars + [2,3,4,5,6,7,8,9]
--- tools_arsene.py
+++ tools_arsene.py
@@ -5,7 +5,7 @@ def ask_for_the_target_base() :
     target_base = input (ask_for_the_target_base_text)
     while not (is_a_valid_base (target_base)) :
         target_base = input(ask_again_for_the_target_base_text)
-    return init_number
+    return target_base
     
     
     
