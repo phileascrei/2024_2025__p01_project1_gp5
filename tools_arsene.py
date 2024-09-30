8dbe5906ed56ef9bb85f44017c7d712053027bdc
arsene432 <arsene.tessier2008@gmail.com>
2024-09-30 15:05:15 +0200

    update data.py et tools_arsene.py
    pregression de la fonction ask_for_the_target_base et ajout de fonction univercelle dans data.py

--- tools_arsene.py
+++ tools_arsene.py
@@ -1,2 +1,12 @@
+from data import *
+
+
 def ask_for_the_target_base() :
-    pass
+    target_base = input (ask_for_the_target_base_text)
+    while not (is_a_valid_base (target_base)) :
+        target_base = input(ask_again_for_the_target_base_text)
+    return init_number
+    
+    
+    
+    
