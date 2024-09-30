eeeac4d98368c1dc0f7f3f2894fd2ed1bc912459
r3nau1 <renaudpointurier1@gmail.com>
2024-09-30 13:50:00 +0200

    Update tools_renaud.py

---
+++ tools_renaud.py
@@ -0,0 +1,7 @@
+
+ask_for_the_init_base_text = "Dans quel base se trouve ton nombre ? "
+
+def ask_for_the_init_base () : 
+    init_base = input(ask_for_the_init_base_text) 
+    
+    return init_base
