ebebbaa76e0df98604a5ce72fb063e4ec26780bd
phileascrei <phileascreignou22@gmail.com>
2024-09-30 13:29:42 +0200

    Update tools_phileas.py
    creation de la fonction is_a_valid_number() et de sa definition

--- tools_phileas.py
+++ tools_phileas.py
@@ -1,2 +1,16 @@
+def is_a_valid_number() :
+    i = 0
+    is_a_valid_char = True
+    while is_a_valid_char() :
+        is_a_valid_char = check_char_number_validity (number [i])
+        i = i + 1
+    return is_a_valid_char
+
+
+
+
+
+
+
 def ask_for_the_init_number () :
     pass
