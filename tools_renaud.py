bfc77fe4532ad91191d1b202ffab73a19e71bb81
r3nau1 <renaudpointurier1@gmail.com>
2024-10-18 12:10:51 +0200

    Update tools_renaud.py
    fin fonction hex_to_bin

--- tools_renaud.py
+++ tools_renaud.py
@@ -5,27 +5,6 @@ def is_a_valid_base (base) :
     return base in bases_accepted
 
 
-
-
-# def ask_for_the_init_base (init_number) : 
-#     init_base = input(ask_for_the_init_base_text)
-    
-#     while True :
-        
-#         if check_validity_of_init_number_for_init_base(init_number) == (True, True, True) and is_a_valid_base(init_base) : 
-#             break
-#         elif check_validity_of_init_number_for_init_base(init_number) == (False, True, True) and is_a_valid_base(init_base): 
-#             if init_base == "2" :
-#                 init_base = input(non_accepted_base)
-#                 break
-#         elif check_validity_of_init_number_for_init_base(init_number) == (False, False, True) and is_a_valid_base(init_base) : 
-#            if init_base in ["2","10"] :
-#                 init_base = input(non_accepted_base)
-#                 break
-    
-#     return init_base 
-
-
 def ask_for_the_init_base(init_number) :
 
     init_base = input(ask_for_the_init_base_text)
@@ -70,3 +49,34 @@ def check_validity_of_init_number_for_init_base (init_number) :
             dec_validity = False
     return bin_validity, dec_validity, hex_validity
 # fonction a rtefaire
+
+
+# rentrer un nombre en base hex
+# qu'on convertit en base bin
+# qui ne comprend que de éléments : 0 et 1 
+# contrairement au nombre entrée qui comprend 16 éléments : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, a, b, c, d, e ou 
+
+# def hex_to_bin (hex_number):
+#     bin_number = 0 
+
+#     return bin_number
+
+# def bin_to_hex (bin_number): 
+#     hex_number = 0
+#     return hex_number
+
+def hex_to_bin(hex_number):
+    bin_number = ''
+
+    for char in hex_number:
+        bin_number += hex_to_bin_map[char]
+    return bin_number
+
+
+
+def bin_to_hex(bin_number):
+    hex_number = ''
+
+    for char in bin_number:
+        hex_number += bin_to_hex_map[char]
+    return hex_number
