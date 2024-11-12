866ffa69bcfe4fb1e5f8b1d717eb9989f199527f
phileascrei <phileascreignou22@gmail.com>
2024-11-12 21:02:38 +0100

    Update main.py
    logique de conversion

--- main.py
+++ main.py
@@ -14,13 +14,37 @@ def do_the_job ():
     target_number = bin_dec_hex_to_bin_dec_hex (init_number, \
                                 init_base,\
                                  target_base)
+    
+    if init_base == "2" :
+        if target_base == "2" :
+            print(init_number)
+        if target_base == "10" :
+            print(bin_to_dec(init_number))
+        if target_base == "16" :
+            print(bin_to_hex(init_number))
+    if init_base == "10" :
+        if target_base == "2" :
+            print(dec_to_bin(init_number))
+        if target_base == "10" :
+            print(init_number)
+        if target_base == "16" :
+            print(dec_to_hex(init_number))
+    if target_base == "16" :
+        if target_base == "2" :
+            print(hex_to_bin(init_number))
+        if target_base == "10" :
+            print(hex_to_dec(init_number))
+        if target_base == "16" :
+            print(init_number)
+        
 
-# do_the_job()
-# print(dec_to_hex("10"))
 
-print(bin_to_dec("101"))      # Sortie attendue : 5
-print(bin_to_dec("1101"))     # Sortie attendue : 13
-print(bin_to_dec("0"))        # Sortie attendue : 0
-print(bin_to_dec("11111111"))  # Sortie attendue : 255
-print(bin_to_dec("10000000"))  # Sortie attendue : 128
-print(bin_to_dec("101")) 
+
+do_the_job()
+
+# print(bin_to_dec("101"))      # Sortie attendue : 5
+# print(bin_to_dec("1101"))     # Sortie attendue : 13
+# print(bin_to_dec("0"))        # Sortie attendue : 0
+# print(bin_to_dec("11111111"))  # Sortie attendue : 255
+# print(bin_to_dec("10000000"))  # Sortie attendue : 128
+# print(bin_to_dec("101")) # Sortie attendue : 5
