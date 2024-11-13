7be02038a4314b0ccf81e9d3f76ad8d3b4e40626
phileascrei <phileascreignou22@gmail.com>
2024-11-13 13:53:00 +0100

    update project
    arrangement et reorganisation des fonction et des data

--- data.py
+++ data.py
@@ -1,8 +1,20 @@
 ask_for_the_init_number_text = "Le nombre ? : "
-ask_again_for_the_init_number_text = "Le nombre put*** ! : "
+ask_again_for_the_init_number_text = "Le nombre entré n'est pas pris en charge par le pogramme, veuillez entrer un autre nombre : "
+
+
+ask_for_the_init_base_intro_text = "Dans quel base se trouve votre nombre ? \n"
+ask_for_the_init_base_exept_bin_and_dec_text = ask_for_the_init_base_intro_text + " Tapez '16' s'il se trouve dans la base Hexadécimale \n"
+ask_for_the_init_base_exept_bin_text = ask_for_the_init_base_exept_bin_and_dec_text + " Tapez '10' s'il se trouve dans la base Decimale \n"
+ask_for_the_init_base_text = ask_for_the_init_base_exept_bin_text + " Tapez '2' s'il se trouve dans la base Binaire \n"
+
+ask_again_for_the_init_base_text = "La base initial entré n'est pas pris en charge par le pogramme, veuillez entrer une autre base : "
+non_accepted_base = "La base n'est pas acceptée pour le nombre entré, veuillez entrer une autre base : "
+
 
 ask_for_the_target_base_text = "La base cible ? : "
-ask_again_for_the_target_base_text = "La base put*** ! : "
+ask_again_for_the_target_base_text = "La base cible entré n'est pas pris en charge par le pogramme, veuillez entrer une autre base : "
+
+
 
 bases_accepted = ["2", "10", "16"]
 
@@ -10,20 +22,10 @@ bin_number_accepted = ["0","1"]
 dec_number_accepted = bin_number_accepted + ["2","3","4","5","6","7","8","9"]
 hex_number_accepted = dec_number_accepted + ["a","b","c","d","e","f"] + ["A","B","C","D","E","F"]
 
-ask_for_the_init_base_intro_text = "Dans quel base se trouve votre nombre ? \n"
-
-ask_for_the_init_base_exept_bin_and_dec_text = ask_for_the_init_base_intro_text + " Tapez '16' s'il se trouve dans la base Hexadécimale \n"
-
-ask_for_the_init_base_exept_bin_text = ask_for_the_init_base_exept_bin_and_dec_text + " Tapez '10' s'il se trouve dans la base Decimale \n"
-
-ask_for_the_init_base_text = ask_for_the_init_base_exept_bin_text + " Tapez '2' s'il se trouve dans la base Binaire \n"
 
 
 
 
-
-ask_again_for_the_init_base_text = "Base initiale eronnée, veuillez réessayer : "
-non_accepted_base = "La base n'est pas acceptée pour le nombre entré, entrer une autre base : "
 
 
 lowercase_alphabet = "abcdefghijklmnopqrstuvwxyz"
--- tools.py
+++ tools.py
@@ -1,27 +1,24 @@
 from data import *
 
 
+"""FONCTIONS PRINCIPALES"""
 
-def ask_for_the_init_number () : 
+def ask_for_the_init_number () : # demande d'entrer un nombre en base binaire, decimal ou hexadacimal
     init_number = input(ask_for_the_init_number_text)
     while not (is_a_valid_number (init_number)) :
         init_number = input(ask_again_for_the_init_number_text)
     return init_number
 
 
-
-
-def ask_for_the_init_base(init_number) :
-
+def ask_for_the_init_base(init_number) : # demande d'entrer la base du nombre donner lors de la fonction ask_for_the_init_number()
     init_base = input(ask_for_the_init_base_text)
 
     while True:
-
         validity = check_validity_of_init_number_for_init_base(init_number)
 
         if is_a_valid_base(init_base):
             if validity == (True, True, True):
-                return init_base
+                return init_base            
             elif validity == (False, True, True):
                 if init_base == "2":
                     init_base = input(non_accepted_base)
@@ -36,14 +33,16 @@ def ask_for_the_init_base(init_number) :
             init_base = input(ask_again_for_the_init_base_text)
 
 
-def ask_for_the_target_base() :
+def ask_for_the_target_base() : # demande d'entrer la base dans laquelle le nombre de la fonction ask_for_the_init_number() a la base entrer dans ask_for_the_init_base() doit etre converti
     target_base = input (ask_for_the_target_base_text)
+
     while not (is_a_valid_base (target_base)) :
         target_base = input(ask_again_for_the_target_base_text)
     return target_base
 
 
-def bin_dec_hex_to_bin_dec_hex (init_number, init_base, target_base) :
+def bin_dec_hex_to_bin_dec_hex (init_number, init_base, target_base) : # converti en fonction des entre de l'utilisateur le nombre en la base voulue
+
     if init_base == "2" :
         if target_base == "2" :
             print(init_number)
@@ -66,7 +65,9 @@ def bin_dec_hex_to_bin_dec_hex (init_number, init_base, target_base) :
         if target_base == "16" :
             print(init_number)
 
-def asking_for_retry () :
+
+def asking_for_retry () : # boucle permettant de demander si l'utilisateur veux refaire une convertion
+
     while True:
         answer = input(retry)
         if answer == "oui" :
@@ -76,7 +77,7 @@ def asking_for_retry () :
             return False
         else:
             print(retry_error)
-        
+ 
 
 
 
@@ -84,15 +85,9 @@ def asking_for_retry () :
 
 
 
+"""CONVERTIONS"""
 
-
-
-
-
-
-
-
-def dec_to_bin(dec_number) :
+def dec_to_bin(dec_number) : # converti un nombre decimal en un nombre binaire
     binary_number = ""
     if is_a_dec_number(dec_number) :
         dec_number = int(dec_number)
@@ -104,7 +99,7 @@ def dec_to_bin(dec_number) :
 
     return binary_number                                                                                                
 
-def bin_to_dec(bin_number):
+def bin_to_dec(bin_number) : # converti un nombre binaire en nmbre decimal
     result = 0
     length = len(bin_number)
 
@@ -113,7 +108,7 @@ def bin_to_dec(bin_number):
         result += bit * (2 ** i) 
     return result
 
-def dec_to_hex(dec_number):
+def dec_to_hex(dec_number) : # converti un nombre decimal en nombre hexadecimal
 
     result = ""
     dec_number = int(dec_number)
@@ -127,7 +122,7 @@ def dec_to_hex(dec_number):
         dec_number = dec_number // 16
     return result
 
-def hex_to_dec(hex_number):
+def hex_to_dec(hex_number) : # converti un nombre hexadecimal en nombre decimal
     hex_number = capitalize(hex_number)
     decimal_value = 0
     length = get_length(hex_number)
@@ -140,7 +135,7 @@ def hex_to_dec(hex_number):
     return str(decimal_value)
 
 
-def hex_to_bin(hex_number):
+def hex_to_bin(hex_number) : # converti un nombre hexadecimal en nombre binaire
 
     if hex_number == "0" :
         return "0"
@@ -149,7 +144,7 @@ def hex_to_bin(hex_number):
         bin_number = dec_to_bin(dec_number)
         return bin_number
 
-def bin_to_hex(bin_number):
+def bin_to_hex(bin_number) : # converti un nombre binaire en un nombre hexadecimal
     
     if bin_number == "0" :
         return "0"
@@ -164,11 +159,7 @@ def bin_to_hex(bin_number):
 
 
 
-
-
-
-
-
+"""VERIFICATIONS"""
 
 def is_a_valid_number(number) : # vérifie si le nombre entrer est accepter.
     i = 0
@@ -196,22 +187,15 @@ def is_a_hex_number(hex_str) :  # vérifie si le nombre entrer est hexadécimale
             return False
     return True
 
+def is_a_valid_base (base) : # verifie si la base entrer fait bien partit des bases gerer par le programme
+    return base in bases_accepted
+
+
+
 def check_char_number_validity (char) : # vérifie si le nombre est valide 
     return char in hex_number_accepted
 
-def get_length(chaine):
-    cpt = 0
-    for char in chaine:
-        cpt += 1
-    return cpt
-
-
-
-
-def is_a_valid_base (base) :
-    return base in bases_accepted
-
-def check_validity_of_init_number_for_init_base (init_number) :
+def check_validity_of_init_number_for_init_base (init_number) : #  vérifie si un nombre est valide pour une base donnée
     bin_validity = True
     dec_validity = True
     hex_validity = True
@@ -228,14 +212,9 @@ def check_validity_of_init_number_for_init_base (init_number) :
 
 
 
+"""UTILITAIRES"""
 
-
-def is_a_valid_base(base) :
-    if base in bases_accepted : 
-        return True
-    return False
-
-def capitalize (chain) :
+def capitalize (chain) : # mette en capital la chaine de caractere entre
     upper_chain = ""
     for char in chain :
         if char in lowercase_alphabet :
@@ -244,7 +223,7 @@ def capitalize (chain) :
             upper_chain += char
     return upper_chain
 
-def get_length(chaine):
+def get_length(chaine) : # donne le nombre de caractere d'une chaine de caractere
     cpt = 0
     for char in chaine:
         cpt += 1
